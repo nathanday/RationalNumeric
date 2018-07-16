ed8e5c8480b738a9c00fe1a3e5ac2549b9cafedc
Nathaniel Day <nathan_day@mac.com>
2018-07-16 22:40:03 +1200

    Initial Commit

---
+++ RationalNumeric/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RationalNumeric
+//
+//  Created by Nathaniel Day on 16/07/18.
+//  Copyright © 2018 Nathaniel Day. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
